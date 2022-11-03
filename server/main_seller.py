79161296c2cbb5147b41e9e7b1896b107df587ea
Sarthak-Singhania-Bungee <sarthak.singhania28@gmail.com>
2022-11-03 13:41:00 +0530

    update

---
+++ server/main_seller.py
@@ -0,0 +1,77 @@
+from flask import Flask, request, make_response
+from flask_cors import CORS, cross_origin
+from flask_mysqldb import MySQL
+from flask_limiter import Limiter, util
+from datetime import datetime, timedelta
+# from jwt import algorithms, ExpiredSignatureError, decode
+from functools import wraps
+from MySQLdb import OperationalError
+import MySQLdb.cursors as cur
+import pandas as pd
+import uuid
+import boto3
+
+application=app=Flask(__name__)
+cros=CORS(app)
+
+limiter = Limiter(
+    application,
+    key_func=util.get_remote_address,
+    default_limits=["200 per day", "50 per hour"],
+    storage_uri="memory://"
+)
+
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'ecommerce'
+app.config['JSON_SORT_KEYS'] = False
+
+app.get('/')
+def home():
+    return make_response({'message':'Home'})
+
+@app.get('/orders')
+def orders_get():
+    try:
+        app.mysql.connection.commit()
+    except OperationalError as SQLdbError:
+        if "Lost connection" not in str(SQLdbError):
+            return SQLdbError.__dict__
+        app.mysql.connection = MySQL(app)
+    seller_id=request.args.get('seller_id')
+    cur = app.mysql.connection.cursor()
+    cur.execute("SELECT * from order join products_in_order using(order_no) join products using(sku) where seller_id='%s'"%(seller_id))
+    data = cur.fetchall()
+    app.mysql.connection.commit()
+    cur.close()
+    return make_response({'data':data}), 200
+
+@app.post('/add_product')
+def add_product():
+    data = request.get_json()
+    sku = data['sku']
+    name = data['name']
+    reg_price = data['reg_price']
+    seller_id = data['seller_id']
+    category = data['category']
+    description = data['description']
+    images = data['images']
+    inventory = data['inventory']
+    try:
+        app.mysql.connection.commit()
+    except OperationalError as SQLdbError:
+        if "Lost connection" not in str(SQLdbError):
+            return SQLdbError.__dict__
+        app.mysql.connection = MySQL(app)
+    cur = app.mysql.connection.cursor()
+    cur.execute("INSERT INTO products VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')"%(sku, name, description, images, reg_price, inventory, seller_id, category))
+    app.mysql.connection.commit()
+    cur.close()
+    return make_response({'message':'Product added'}), 200
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=5002,debug=True)
---
+++ server/main_user.py
@@ -0,0 +1,289 @@
+from flask import Flask, request, make_response
+from flask_cors import CORS, cross_origin
+from flask_mysqldb import MySQL
+from flask_limiter import Limiter, util
+from datetime import datetime, timedelta
+# from jwt import algorithms, ExpiredSignatureError, decode
+from functools import wraps
+from MySQLdb import OperationalError
+import MySQLdb.cursors as cur
+import pandas as pd
+import uuid
+
+application=app=Flask(__name__)
+cros=CORS(app)
+
+limiter = Limiter(
+    application,
+    key_func=util.get_remote_address,
+    default_limits=["200 per day", "50 per hour"],
+    storage_uri="memory://"
+)
+
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'ecommerce'
+app.config['JSON_SORT_KEYS'] = False
+
+app.get('/')
+def home():
+    return make_response({'message':'Home'})
+
+# @app.post('/signup')
+# def signup():
+#     data = request.get_json()
+#     username = data['username']
+#     password = data['password']
+#     email = data['email']
+#     phoneno=data['phoneno']
+#     try:
+#         app.mysql.connection.commit()
+#     except OperationalError as SQLdbError:
+#         if "Lost connection" not in str(SQLdbError):
+#             return SQLdbError.__dict__
+#         app.mysql.connection = MySQL(app)
+#     cur = app.mysql.connection.cursor()
+#     cur.execute("INSERT INTO users VALUES('%s', '%s', '%s', '%s')"%(username, password, email, phoneno))
+#     app.mysql.connection.commit()
+#     cur.close()
+#     return make_response({'message':'Registered'}), 200
+
+# @app.post('/login')
+# def login():
+#     data = request.get_json()
+#     username = data['username']
+#     password = data['password']
+#     try:
+#         app.mysql.connection.commit()
+#     except OperationalError as SQLdbError:
+#         if "Lost connection" not in str(SQLdbError):
+#             return SQLdbError.__dict__
+#         app.mysql.connection = MySQL(app)
+#     cur = app.mysql.connection.cursor()
+#     cur.execute("SELECT * FROM users WHERE username = '%s' AND password = '%s'"%(username, password))
+#     result = cur.fetchone()
+#     cur.close()
+#     if result:
+#         return make_response({'message':'Logged in'}), 200
+#     else:
+#         return make_response({'message':'Invalid credentials'}), 401
+
+@app.get('/products')
+def products():
+    try:
+        app.mysql.connection.commit()
+    except OperationalError as SQLdbError:
+        if "Lost connection" not in str(SQLdbError):
+            return SQLdbError.__dict__
+        app.mysql.connection = MySQL(app)
+    cur = app.mysql.connection.cursor()
+    if request.args.get('sku'):
+        cur.execute('select * from products where sku = %s', (request.args.get('sku'),))
+        result=cur.fetchone()
+        if result:
+            if result['promo_price']==0:
+                del result['promo_price']
+            cur.execute('select avg(rating) as rating from reviews where sku = %s', (request.args.get('sku'),))
+            rating=cur.fetchone()
+            if rating['rating']:
+                result['rating']=rating['rating']
+            cur.execute('select * from reviews where sku = %s and username = %s', (request.args.get('sku'),request.args.get('username')))
+            review=cur.fetchone()
+            if review:
+                result['review']=review['rating']
+            cur.close()
+            return make_response({'message':'Product found', 'product':result}), 200
+        else:
+            return make_response({'message':'Product not found'}), 404
+    else:
+        cur.execute('select * from products')
+        result=cur.fetchall()
+        cur.close()
+        if result:
+            result['images']=eval(result['images'])
+            return make_response({'message':'Products found', 'products':result}), 200
+        else:
+            return make_response({'message':'Products not found'}), 404
+
+@app.get('/category')
+def category():
+    try:
+        app.mysql.connection.commit()
+    except OperationalError as SQLdbError:
+        if "Lost connection" not in str(SQLdbError):
+            return SQLdbError.__dict__
+        app.mysql.connection = MySQL(app)
+    cur = app.mysql.connection.cursor()
+    if request.args.get('category'):
+        cur.execute('select * from products where category = %s', (request.args.get('category'),))
+        result=cur.fetchall()
+        cur.close()
+        if result:
+            return make_response({'message':'Products found', 'products':result}), 200
+        else:
+            return make_response({'message':'Products not found'}), 404
+    else:
+        return make_response({'message':'Category not found'}), 404
+
+@app.get('/search')
+def search():
+    try:
+        app.mysql.connection.commit()
+    except OperationalError as SQLdbError:
+        if "Lost connection" not in str(SQLdbError):
+            return SQLdbError.__dict__
+        app.mysql.connection = MySQL(app)
+    cur = app.mysql.connection.cursor()
+    if request.args.get('query'):
+        cur.execute('select * from products where name like %s', ('%'+request.args.get('query')+'%',))
+        result=cur.fetchall()
+        cur.close()
+        if result:
+            return make_response({'message':'Products found', 'products':result}), 200
+        else:
+            return make_response({'message':'Products not found'}), 404
+    else:
+        return make_response({'message':'Query not found'}), 404
+
+@app.get('/cart')
+def cart_get():
+    try:
+        app.mysql.connection.commit()
+    except OperationalError as SQLdbError:
+        if "Lost connection" not in str(SQLdbError):
+            return SQLdbError.__dict__
+        app.mysql.connection = MySQL(app)
+    cur = app.mysql.connection.cursor()
+    if request.args.get('username'):
+        cur.execute('select * from cart join products using(sku) where username = %s', (request.args.get('username'),))
+        result=cur.fetchall()
+        cur.close()
+        if result:
+            return make_response({'message':'Cart found', 'cart':result}), 200
+        else:
+            return make_response({'message':'Cart not found'}), 404
+    else:
+        return make_response({'message':'Username not found'}), 404
+
+@app.post('/cart')
+def cart_post():
+    data = request.get_json()
+    username = data['username']
+    sku = data['sku']
+    quantity = data['quantity']
+    try:
+        app.mysql.connection.commit()
+    except OperationalError as SQLdbError:
+        if "Lost connection" not in str(SQLdbError):
+            return SQLdbError.__dict__
+        app.mysql.connection = MySQL(app)
+    cur = app.mysql.connection.cursor()
+    cur.execute("INSERT INTO cart VALUES('%s', '%s', '%s')"%(username, sku, quantity))
+    app.mysql.connection.commit()
+    cur.close()
+    return make_response({'message':'Added to cart'}), 200
+
+@app.delete('/cart')
+def cart_delete():
+    data = request.get_json()
+    username = data['username']
+    sku = data['sku']
+    try:
+        app.mysql.connection.commit()
+    except OperationalError as SQLdbError:
+        if "Lost connection" not in str(SQLdbError):
+            return SQLdbError.__dict__
+        app.mysql.connection = MySQL(app)
+    cur = app.mysql.connection.cursor()
+    cur.execute("DELETE FROM cart WHERE username = '%s' AND sku = '%s'"%(username, sku))
+    app.mysql.connection.commit()
+    cur.close()
+    return make_response({'message':'Deleted from cart'}), 200
+
+@app.get('/orders')
+def orders_get():
+    try:
+        app.mysql.connection.commit()
+    except OperationalError as SQLdbError:
+        if "Lost connection" not in str(SQLdbError):
+            return SQLdbError.__dict__
+        app.mysql.connection = MySQL(app)
+    cur = app.mysql.connection.cursor()
+    if request.args.get('username'):
+        cur.execute('select * from orders where username = %s', (request.args.get('username'),))
+        result=cur.fetchall()
+        cur.close()
+        if result:
+            return make_response({'message':'Orders found', 'orders':result}), 200
+        else:
+            return make_response({'message':'Orders not found'}), 404
+    else:
+        return make_response({'message':'Username not found'}), 404
+
+@app.post('/order')
+def order_post():
+    data = request.get_json()
+    date=datetime.datetime.now()
+    username = data['username']
+    products = data['products']
+    quantity = data['quantity']
+    order_id=uuid.uuid4()
+    try:
+        app.mysql.connection.commit()
+    except OperationalError as SQLdbError:
+        if "Lost connection" not in str(SQLdbError):
+            return SQLdbError.__dict__
+        app.mysql.connection = MySQL(app)
+    cur = app.mysql.connection.cursor()
+    cur.execute("INSERT INTO orders VALUES('%s', '%s', '%s', '%s')"%(order_id,username, quantity, date))
+    app.mysql.connection.commit()
+    for i in products:
+        cur.execute("INSERT INTO products_in_order VALUES('%s', '%s')"%(order_id, i))
+        app.mysql.connection.commit()
+    cur.close()
+    return make_response({'message':'Added to orders'}), 200
+
+@app.get('/order')
+def order_get():
+    try:
+        app.mysql.connection.commit()
+    except OperationalError as SQLdbError:
+        if "Lost connection" not in str(SQLdbError):
+            return SQLdbError.__dict__
+        app.mysql.connection = MySQL(app)
+    cur = app.mysql.connection.cursor()
+    if request.args.get('order_id'):
+        cur.execute('select * from orders where order_id = %s', (request.args.get('order_id'),))
+        result=cur.fetchall()
+        cur.execute('select * from products_in_order where order_id = %s', (request.args.get('order_id'),))
+        result1=cur.fetchall()
+        cur.close()
+        if result and result1:
+            return make_response({'message':'Order found', 'order_details':result, 'order_products':result1}), 200
+        else:
+            return make_response({'message':'Order not found'}), 404
+    else:
+        return make_response({'message':'Order id not found'}), 404
+
+@app.post('/review')
+def review_post():
+    data = request.get_json()
+    username = data['username']
+    sku = data['sku']
+    review = data['review']
+    try:
+        app.mysql.connection.commit()
+    except OperationalError as SQLdbError:
+        if "Lost connection" not in str(SQLdbError):
+            return SQLdbError.__dict__
+        app.mysql.connection = MySQL(app)
+    cur = app.mysql.connection.cursor()
+    cur.execute("INSERT INTO reviews (username, sku, rating) VALUES('%s', '%s', '%s')"%(username, sku, review))
+    app.mysql.connection.commit()
+    cur.close()
+    return make_response({'message':'Added to reviews'}), 200
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=5001,debug=True)

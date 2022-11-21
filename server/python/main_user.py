9f644f7a2d426d5b08859363ea821c84c7fc573a
Sarthak-Singhania <sarthak.singhania28@gmail.com>
2022-11-21 14:55:28 +0530

    update

--- server/python/main_user.py
+++ server/python/main_user.py
@@ -164,6 +164,7 @@ def cart_get():
         result = cur.fetchall()
         cur.close()
         if result:
+            result['Images']=eval(result['Images'][:1])
             return make_response({'message': 'Cart found', 'cart': result}), 200
         else:
             return make_response({'message': 'Cart not found'}), 404
